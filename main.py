76aa8b39bbae7d8ac13e612d3c43026cf91d3290
hanseo <hanseok631@gmail.com>
2026-09-15 13:09:52 +0900

    Every error handling done, fully finished. Code refactoring left

--- main.py
+++ main.py
@@ -12,8 +12,13 @@ DummyValue = {
 def main():
     PrevActionType = "None"
     data = []
-    with open("Saved/expenses.json", "r") as file:
-        data = json.load(file)
+    try:
+        with open("Saved/expenses.json", "r") as file:
+            data = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        print("The JSON file is corrupted or doesn't exist?")
+        data = []
+
     ProgramRunning = True
 
     while ProgramRunning:
@@ -24,6 +29,11 @@ def main():
             Instruction = "None"
         elif len(TempList) == 2:
             ActionType, Instruction = TempList
+
+        if not ActionType in {"add", "remove", "list", "edit", "total"}:
+            print("This is not a valid action!")
+            continue
+
         SaveCandidate = []
         if PrevActionType == "remove" and ActionType != "remove":
             PrevActionType = "None"
@@ -36,19 +46,27 @@ def main():
                 json.dump(data, file)
 
         if ActionType == "add":
-            amount, category, description = Instruction.split(" ", 2)
-            NewVal = {
-                "amount": amount,
-                "category": category,
-                "description": description,
-            }
-            data.append(NewVal)
-            Utilities.printlist(data)
-
+            try:
+                amount, category, description = Instruction.split(" ", 2)
+                try:
+                    NewVal = {
+                        "amount": int(amount),
+                        "category": category,
+                        "description": description,
+                    }
+                    data.append(NewVal)
+                    Utilities.printlist(data)
+                except ValueError:
+                    print("Invalid Integer!")
+            except ValueError:
+                print("Insufficient Arguments!")
         elif ActionType == "remove":
-            data[int(Instruction) - 1] = DummyValue.copy()
-            PrevActionType = "remove"
-            print(f"Removed number {Instruction} from the list")
+            try:
+                data[int(Instruction) - 1] = DummyValue.copy()
+                PrevActionType = "remove"
+                print(f"Removed number {Instruction} from the list")
+            except ValueError:
+                print("Invalid Integer!")
 
         elif ActionType == "list":
             Utilities.printlist(data)
@@ -56,24 +74,29 @@ def main():
                 json.dump(data, file)
 
         elif ActionType == "edit":
-            TargetNum, amount, category, description = Instruction.split(" ", 3)
-            print(
-                f"Edited {data[int(TargetNum)-1]['amount']} >> {amount}, {data[int(TargetNum)-1]['category']} >> {category}, {data[int(TargetNum)-1]['description']} >> {description},"
-            )
-            data[int(TargetNum) - 1] = {
-                "amount": amount,
-                "category": category,
-                "description": description,
-            }
-            with open("Saved/expenses.json", "w") as file:
-                json.dump(data, file)
-
+            try:
+                TargetNum, amount, category, description = Instruction.split(" ", 3)
+                try:
+                    print(
+                        f"Edited {data[int(TargetNum)-1]['amount']} >> {amount}, {data[int(TargetNum)-1]['category']} >> {category}, {data[int(TargetNum)-1]['description']} >> {description},"
+                    )
+                    data[int(TargetNum) - 1] = {
+                        "amount": amount,
+                        "category": category,
+                        "description": description,
+                    }
+                    with open("Saved/expenses.json", "w") as file:
+                        json.dump(data, file)
+                except ValueError:
+                    print("Invalid integer!")
+            except ValueError:
+                print("Insufficient Arguments!")
         elif ActionType == "total":
             if Instruction == "None":
                 print("count total ")
                 Sum = 0
                 for log in data:
-                    Sum += int(log["amount"])
+                    Sum += log["amount"]
                 print(f"The total is {Sum}!")
 
             elif Instruction != "None":
@@ -86,11 +109,8 @@ def main():
                         data[i]["description"],
                     )
                     if category == Instruction:
-                        Sum += int(amount)
+                        Sum += amount
                 print(f"The total for {Instruction} is {Sum}!")
-        else:
-            print("This is not a valid action")
-            continue
         Confirm = input("Do you have more to log?(y/n)")
 
         if Confirm == "y":
